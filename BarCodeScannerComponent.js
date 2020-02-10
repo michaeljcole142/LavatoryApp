66e0b6f61d05b5e06732cc88a5e6dd298f42b1f1
michael.cole <michaeljcole142@gmail.com>
2020-02-10 16:58:29 -0500

    adding

--- BarCodeScannerComponent.js
+++ BarCodeScannerComponent.js
@@ -2,7 +2,22 @@ import React, { Component } from 'react';
 import { Text, View, Button,StyleSheet } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera } from 'expo-camera';
+import { Audio } from 'expo-av';
 
+/* 
+ * This is camera access authorization. 
+ * Keeping log print for debugging as we make this an app.
+ *
+ */
+console.log("Permissions = ");
+console.log(Permissions.CAMERA); 
+import auth from './auth.json';
+
+
+/*
+ * This function is used to call the server for a checkin/checkout style toggle of a student.
+ */
  
 async function checkInOrOut(data,server,port,ilav,name) {
 	console.log("in checkin");
@@ -36,9 +51,11 @@ export default class BarCodeScannerComponent extends React.Component {
 		this.state = {
 			hasCameraPermission:null,
 			scanned :false ,
+			type: Camera.Constants.Type.back,
 			checkedInOrOutFlag:false,
 			checkedInOrOutMsg : ''
 		}
+		console.log("type state is ", this.state.type);
 	};
 	
 	/* get permissions for camera after component mounts. */
@@ -50,8 +67,10 @@ export default class BarCodeScannerComponent extends React.Component {
 	getPermissionsAsync = async () => {
 		const { status } = await Permissions.askAsync(Permissions.CAMERA);
 		this.setState({ hasCameraPermission: status === 'granted' });
+		
 	};
 
+	
 	/* paint the screen.  gets called when props or state change.*/
 	render() {
 		const { hasCameraPermission, scanned } = this.state;
@@ -78,7 +97,20 @@ export default class BarCodeScannerComponent extends React.Component {
 				<View style={{ flex: 6 }}>
 					<BarCodeScanner
 						onBarCodeScanned={this.state.scanned ? undefined : this.handleBarCodeScanned}
+						type={this.state.type}
+		
 						style={StyleSheet.absoluteFillObject}
+					/>
+					<Button 
+						style= {{ flex:1, justifyContent: 'flex-end', flexDirection: 'column' }}
+						title='Change Camera'
+						onPress={ ()=> { 
+							if (this.state.type == Camera.Constants.Type.front ) {
+								this.setState({type:Camera.Constants.Type.back})
+							} else {
+								this.setState({type: Camera.Constants.Type.front})
+							}
+						}}
 					/>
 				</View>
 				<View style={{flex:1}}>
@@ -108,8 +140,17 @@ export default class BarCodeScannerComponent extends React.Component {
 	handleBarCodeScanned = ({ type, data }) => {
 		this.setState({ scanned: true });
 		const rr = async()=> { 
-			var msg=await checkInOrOut(data,this.props.getServer(),this.props.getPort(),this.props.getLav(),this.props.getMyName());
-			this.setState( { checkedInOrOutFlag: true , checkedInOrOutMsg : msg });
+			try {
+				var msg=await checkInOrOut(data,this.props.getServer(),this.props.getPort(),this.props.getLav(),this.props.getMyName());
+				this.setState( { checkedInOrOutFlag: true , checkedInOrOutMsg : msg });
+				const soundObject = new Audio.Sound();
+				await soundObject.loadAsync(require('./assets/sounds/tone.wav'));
+				await soundObject.playAsync();
+				// Your sound is playing!
+			} catch (error) {
+				// An error occurred!
+				console.log("crud an error happened!" , error);
+			}
 		}
 		rr();
 	};
